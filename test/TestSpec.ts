7ff630870ed999b5612c8ded06c73f5acf7fca47
codelogicws <russ@codelogic.ws>
2016-04-30 21:18:22 -0700

    Typings are installed now for typescript

--- test/TestSpec.ts
+++ test/TestSpec.ts
@@ -13,15 +13,7 @@ var expect = chai.expect;
  */
 describe('User Model Unit Tests:', () => {
 
-    describe('2 + 4', () => {
-        it('should be 6', (done) => {
-            expect(2+4).to.equals(6);
-            done();
-        });
-
-        it('should not be 7', (done) => {
-            expect(2+4).to.not.equals(7);
-            done();
-        });
-    });
+  it('should execute test', function(){
+    expect(2+4).to.equals(6);
+  });
 });
